e0e4f7fda556b243d4b30c4c2a15f891450fa93b
Billy Bunn <williammbunn@gmail.com>
2019-03-19 16:08:00 -0700

    Adds code challenge in code, all tests passing

--- code-challenges/401/arrayShift/array-shift.js
+++ code-challenges/401/arrayShift/array-shift.js
@@ -1,9 +1,24 @@
 'use strict';
 
 module.exports = exports = (arr, val) => {
+  // validates input for nothing passed in, only one parameter passed in, or the first parameter passed in not being an array - returns null
   if (!arr || !val || !Array.isArray(arr)) return null;
 
-  
+  let newArr = [];
+  newArr.length = arr.length + 1;
 
-  return arr;
+  const midPoint = Math.ceil(arr.length / 2); // rounds up for odd numbered array lengths
+
+  // fills the first half of the new array
+  for (let i = 0; i < midPoint; i++) {
+    newArr[i] = arr[i];
+  }
+  // assigns value passed in to the midpoint of the new array
+  newArr[midPoint] = val;
+  // fills the second half of the new array
+  for (let i = midPoint + 1; i < newArr.length; i++) {
+    newArr[i] = arr[i - 1];
+  }
+  
+  return newArr;
 };
