a2aa5ba05613e771b11e7a553f5a7defdec3706c
Billy Bunn <williammbunn@gmail.com>
2019-03-19 08:24:53 -0700

    Restructures repo folders. Adds tests to arrayReverse challenge

--- code-challenges/401/arrayReverse/array-reverse.js
+++ code-challenges/401/arrayReverse/array-reverse.js
@@ -1,8 +1,10 @@
-// INPUT array, OUTPUT array with elements in reverse order
-function reverseArray(arr) {
+'use strict';
+
+module.exports = exports = (arr) => {
+  if (!Array.isArray(arr)) return null;
   let result = [];
   for (let i = arr.length - 1; i >= 0; i--) {
     result.push(arr[i]);
   }
   return result;
-}
+};
---
+++ linked-lists/__tests__/linked-lists.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('linked lists', () => {
+  it('tests go here', () => {
+
+  });
+});
