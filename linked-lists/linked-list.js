552d3d56f4a08f63e6f83bdea25fd247b82ba177
Billy Bunn <williammbunn@gmail.com>
2019-03-23 19:50:30 -0700

    adds includes method

--- linked-lists/linked-list.js
+++ linked-lists/linked-list.js
@@ -13,6 +13,7 @@ class LinkedList {
   constructor() {
     this.head = null; // when list is empty, the head is null
   }
+
   // method to add a node at the head of the list
   insert(value) {
     let node = new Node(value);
@@ -20,33 +21,50 @@ class LinkedList {
     this.head = node; // make the new node the the first node (the head)
     return this.head; // return the new list
   }
+
   // method to add a node at the end of the list
   add(value) {
     let node = new Node(value); // creae a new node with whatever value
 
     if (this.head === null) { // if the list is empty
-      this.head = node;
-      return this.head;
+      this.head = node; // make the new node the head
+      return this.head; // return the new list
     }
-
     let current = this.head; // starting with the first node
-    while (current.next !== null) { // while we're not on the last node, loop through the list
-      current = current.next;
+    while (current.next !== null) { // while we're not on the last node
+      current = current.next; // go down the list
     } // stops when we're on the last node in the list
     current.next = node; // add the new node to the end of the list
     return this.head; // return the new list
   }
+
+  // method to return true/false if a value is found in the list
+  includes(value) {
+    let current = this.head; // starting at the first node
+    while (current !== null) { // while we're not at the end of the list
+      if (current.value === value) { // if the current node is what we're looking for, "true"
+        return true;
+      } else {
+        current = current.next; // otherwise, go to the next node
+      }
+    }
+    return false; // if we loop through without finding the value, "false"
+  }
+
 }
 
 let list = new LinkedList(); //?
-list.insert('SOME VALUE BRO');
-// list.insert('ANOTHER'); //?
-list.add('THIRD');
+list.add('FIRST'); //?
+list.add('SECOND'); //?
+list.add('THIRD'); //?
+list.insert('ZERO'); //?
+
+
 console.log(list);
 // list.head.next.next; //?
+list.includes('THIRD'); //?
 
-
-list; 
+list;
 
 
 
@@ -67,16 +85,9 @@ list;
 
 /*
 Features
-Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
-Within your LinkedList class, include a head property. Upon instantiation, an empty Linked List should be created.
-
-This object should be aware of a default empty value assigned to head when the linked list is instantiated.
-
-Define a method called insert which takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance.
-
-Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
 
 Define a method called print which takes in no arguments and returns a collection all of the current Node values in the Linked List.
+
 At no time should an exception or stack trace be shown to the end user. Catch and handle any such exceptions and return a printed value or operation which cleanly represents the state and either stops execution cleanly, or provides the user with clear direction and output.
 Be sure to follow your language/frameworks standard naming conventions (e.g. C# uses PascalCasing for all method and class names).
 
