9fea633a6ad5c0b9eeca1e4e8fda97f443b08c29
Billy Bunn <williammbunn@gmail.com>
2019-03-23 18:15:36 -0700

    sets up linked list with lecture instructions

---
+++ linked-lists/linked-list.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Node class
+class Node {
+  constructor(value) {
+    this.value = value;
+    // this.next = ?????
+  }
+}
+
+// Linked list class
+
+
+
+// Add method - to the end
+// Insert method - to the beginning
+// Print method
