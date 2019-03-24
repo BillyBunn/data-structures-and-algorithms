deec0ec9803d1a98fdcb6876a33bdb52697f6219
Billy Bunn <williammbunn@gmail.com>
2019-03-23 20:07:08 -0700

    adds print method

--- linked-lists/linked-list.js
+++ linked-lists/linked-list.js
@@ -26,12 +26,12 @@ class LinkedList {
   add(value) {
     let node = new Node(value); // creae a new node with whatever value
 
-    if (this.head === null) { // if the list is empty
+    if (!this.head) { // if the list is empty
       this.head = node; // make the new node the head
       return this.head; // return the new list
     }
     let current = this.head; // starting with the first node
-    while (current.next !== null) { // while we're not on the last node
+    while (current.next) { // while we're not on the last node
       current = current.next; // go down the list
     } // stops when we're on the last node in the list
     current.next = node; // add the new node to the end of the list
@@ -41,7 +41,7 @@ class LinkedList {
   // method to return true/false if a value is found in the list
   includes(value) {
     let current = this.head; // starting at the first node
-    while (current !== null) { // while we're not at the end of the list
+    while (current) { // while we're not at the end of the list
       if (current.value === value) { // if the current node is what we're looking for, "true"
         return true;
       } else {
@@ -51,6 +51,16 @@ class LinkedList {
     return false; // if we loop through without finding the value, "false"
   }
 
+  // method to return an array of node values
+  print() {
+    let values = [];
+    let current = this.head;
+    while (current) { // while we're not at the end of the list
+      values.push(current.value); // add the current node's value to the array
+      current = current.next; // go to the next node
+    }
+    return values; // return the filled array of values
+  }
 }
 
 let list = new LinkedList(); //?
@@ -63,6 +73,7 @@ list.insert('ZERO'); //?
 console.log(list);
 // list.head.next.next; //?
 list.includes('THIRD'); //?
+list.print(); //?
 
 list;
 
