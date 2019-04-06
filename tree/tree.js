fc5f141108cb7dcb0773791e1a07fc65367abdaf
Billy Bunn <williammbunn@gmail.com>
2019-04-06 13:35:23 -0700

    adds binary tree traversal methods

--- tree/tree.js
+++ tree/tree.js
@@ -13,19 +13,43 @@ class BinaryTree {
     this.root = root;
   }
 
-  preOrder() {
-    // returns an array of the values, ordered appropriately
+  preOrder(node = this.root, values = []) {
+    // INPUT <-- root Node
+    // OUTPUT <-- preorder output of tree nodes
 
+    values.push(node.value);
+
+    if (node.leftChild) this.preOrder(node.leftChild, values);
+
+    if (node.rightChild) this.preOrder(node.rightChild, values);
+
+    return values;
   }
 
-  inOrder() {
-    // returns an array of the values, ordered appropriately
+  inOrder(node = this.root, values = []) {
+    // INPUT <-- root Node
+    // OUTPUT <-- inorder output of tree nodes
 
+    if (node.leftChild) this.inOrder(node.leftChild, values);
+
+    values.push(node.value);
+
+    if (node.rightChild) this.inOrder(node.rightChild, values);
+
+    return values;
   }
 
-  postOrder() {
-    // returns an array of the values, ordered appropriately
+  postOrder(node = this.root, values = []) {
+    // INPUT <-- root Node
+    // OUTPUT <-- postorder output of tree nodes
 
+    if (node.leftChild) this.postOrder(node.leftChild, values);
+
+    if (node.rightChild) this.postOrder(node.rightChild, values);
+
+    values.push(node.value);
+
+    return values;
   }
 }
 
@@ -39,8 +63,23 @@ class BinarySearchTree extends BinaryTree {
   }
 }
 
-let root = new Node(1);
+let a = new Node('A');
+let b = new Node('B');
+let c = new Node('C');
+let d = new Node('D');
+let e = new Node('E');
+let f = new Node('F');
 
-let tree = new BinaryTree(root);
+let tree = new BinaryTree(a);
 
 tree;
+
+tree.root.leftChild = b;
+tree.root.rightChild = c;
+tree.root.leftChild.leftChild = d;
+tree.root.leftChild.rightChild = e;
+tree.root.rightChild.leftChild = f;
+
+tree.preOrder(); //?
+tree.inOrder(); //?
+tree.postOrder(); //?
