8e8cf52e745d4c64df6b913ebfb97b7ab181a20d
Billy Bunn <williammbunn@gmail.com>
2019-04-22 13:05:34 -0700

    adds tests

---
+++ sort/__tests__/insertion-sort.test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+describe('insertion-sort.js', () => {
+
+  it('A randomly generated unsorted array returns the array sorted', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('A sorted array returns the same sorted array', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('A backwards-sorted array returns the array sorted', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('An empty array returns the same empty array', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('An array of one element returns the same single-element array', () => {
+    expect(true).toBeTruthy();
+  });
+
+});
+
+/*
+A randomly generated unsorted array returns the array sorted
+A sorted array returns the same sorted array
+A backwards-sorted array returns the array sorted
+An empty array returns the same empty array
+An array of one element returns the same single-element array
+*/
