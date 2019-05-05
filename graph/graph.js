9fcae93f29a215c5d3418218544eca94b7826f1c
Billy Bunn <williammbunn@gmail.com>
2019-05-05 14:16:24 -0700

    adds getNeighbors and print methods

--- graph/graph.js
+++ graph/graph.js
@@ -36,13 +36,38 @@ class Graph {
   }
 
   getVertices() {
-    let vertices = [];
-    let keys = this.adjacencyList.keys();
-    for(let value of keys) {
-      vertices.push(value);
-    }
-    return vertices;
+    // let vertices = [];
+    // let keys = this.adjacencyList.keys();
+    // for(let value of keys) {
+    //   vertices.push(value);
+    // }
+    // return vertices;
+
+    return this.adjacencyList.values();
   }
 
-  
+  getNeighbors(vertex) {
+    return this.adjacencyList.get(vertex);
+  }
+
+  print() {
+    for (const [key, value] of this.adjacencyList) {
+      console.log(key, value);
+    }
+  }
 }
+
+const graph = new Graph();
+let a = graph.addVertex('Washington');
+let b = graph.addVertex('North Dakota');
+let c = graph.addVertex('California');
+
+graph.addUndirectedEdge(a, b, 10);
+graph.addUndirectedEdge(b, c, 100);
+graph.addUndirectedEdge(a, c, 9);
+
+// graph.print();
+
+graph.getVertices(); //?
+
+// graph.getNeighbors(b); //?
