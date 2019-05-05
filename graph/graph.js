99433f8665152e5ed06f2f91a6a0f4bec433d238
Billy Bunn <williammbunn@gmail.com>
2019-05-05 13:55:34 -0700

    adds a getAllVertices method

--- graph/graph.js
+++ graph/graph.js
@@ -6,17 +6,42 @@ class Vertex {
   }
 }
 
+class Edge {
+  constructor(vertex, weight) {
+    this.vertex = vertex;
+    this.weight = weight;
+  }
+}
+
 class Graph {
   constructor() {
     this.adjacencyList = new Map();
     this._size = 0;
   }
-  
+
   addVertex(value) {
     let vertex = new Vertex(value);
     this.adjacencyList.set(vertex, []);
     this._size++;
     return vertex;
+  }
+
+  addDirectedEdge(startVertex, endVertex, weight) {
+    this.adjacencyList.get(startVertex).push(new Edge(endVertex, weight));
+  }
+
+  addUndirectedEdge(startVertex, endVertex, weight) {
+    this.addDirectedEdge(startVertex, endVertex, weight);
+    this.addDirectedEdge(endVertex, startVertex, weight);
+  }
+
+  getVertices() {
+    let vertices = [];
+    let keys = this.adjacencyList.keys();
+    for(let value of keys) {
+      vertices.push(value);
+    }
+    return vertices;
   }
 
   
