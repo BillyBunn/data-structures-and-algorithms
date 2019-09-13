359a74d982d323c8c6a903afd7b7c45a88fea540
Billy Bunn <williammbunn@gmail.com>
2019-09-12 19:38:50 -0700

    starts new mergesort

---
+++ code-challenges/401n13/merge-sort/merger-sort-2.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function mergeSort(items) {
+  // Find the midpoint and split the items into two sides
+  // Call mergeSort on the left side
+  // Call mergeSort on the right side
+  // Merge the two halves
+}
